c8c2141a6d76f65f2acaec67504d2e335f041514
cecyliaborek <46850757+cecyliaborek@users.noreply.github.com>
2019-05-05 20:11:48 +0200

    Add files via upload

---
+++ template - zad3.py
@@ -0,0 +1,17 @@
+import tkinter
+
+
+class MyGUI:
+    def __init__(self, master):
+        #Inicjacja okna
+
+        #Tworzenie buttona
+
+    def greet(self):
+        self.label = tkinter.Label(self.master, text="hello world")
+        self.label.pack()
+
+
+#Utworzenie root'a
+#Stworzenie instancji klasy MyGUI
+#Pętla główna
