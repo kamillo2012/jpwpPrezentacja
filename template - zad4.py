fe36d236a0206f987bc54d00e2948a52cde1e2bf
cecyliaborek <46850757+cecyliaborek@users.noreply.github.com>
2019-05-05 20:14:48 +0200

    Add files via upload

---
+++ template - zad4.py
@@ -0,0 +1,13 @@
+import time
+from threading import Thread
+
+
+def myfunc(i):
+    #Wyświetlanie powitania
+    #Czekanie 5 sekund
+    #Wyświetlanie pożegnania
+
+
+for i in range(10):
+    #Utworzenie wątku
+    #Uruchomienie wątku
